127ea0293ae3606002f41913d30ad4ba9ed6a207
1E10 Laptop <sample@bths.edu>
2023-01-11 11:03:34 -0500

    Step 1

--- src/main/java/com/example/atm/SceneController.java
+++ src/main/java/com/example/atm/SceneController.java
@@ -1,2 +1,37 @@
-package com.example.atm;public class SceneController {
+package com.example.atm;
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Group;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+
+import java.io.IOException;
+public class SceneController {
+    private Stage stage;
+    public void switchToMainMenu(ActionEvent event) throws IOException {
+        Group root = new Group();
+        Scene menu = new Scene(root);
+        stage.setScene(menu);
+        stage.show();
+    }
 }
